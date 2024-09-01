7e3d1d22af3a2186f49156aedf2c7e760002c774
Vladimir Haiduk <nightshade2903@gmail.com>
2024-09-01 16:55:56 +0300

    feature-BJS2-25876
    rebase AccountType;

--- src/main/java/faang/school/accountservice/enums/AccountType.java
+++ src/main/java/faang/school/accountservice/enums/AccountType.java
@@ -1,13 +1,17 @@
 package faang.school.accountservice.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum AccountType {
     CREDIT(5236),
     DEPOSIT(4200),
-    DEBIT(7230);
+    DEBIT(7230),
+    CHECKING(8800),
+    FOREIGN_CURRENCY(1231),
+    BUSINESS(3340);
 
     private final int prefixCode;
-    CHECKING,
-    SAVINGS,
-    FOREIGN_CURRENCY,
-    BUSINESS
 }
